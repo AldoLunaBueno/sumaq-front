7e4ed3d323d32525ed739295b6283002fe8b83e5
AldoLunaBueno <albanovaphi7@gmail.com>
2025-08-12 05:00:27 -0500

    fix websocket endpoint

--- src/hooks/useSensorData.ts
+++ src/hooks/useSensorData.ts
@@ -40,7 +40,7 @@ export function useSensorData() {
   }, []);
 
   useEffect(() => {
-    const ws = new WebSocket(`${apiUrl.replace(/^https/, "wss")}/wss`);
+    const ws = new WebSocket(`${apiUrl.replace(/^https/, "wss")}/ws`);
 
     ws.onmessage = (event) => {
       try {
